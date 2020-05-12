06b9c69f058d09fb46df7937dc0c4599e178f642
Eric Liu <ericyl.us@gmail.com>
2020-05-11 19:45:59 -0700

    docs: support mobile menu [ci skip]

--- demo/src/routes/_layout.svelte
+++ demo/src/routes/_layout.svelte
@@ -2,12 +2,15 @@
   export let segment = undefined;
 
   import * as Primer from "svelte-primer";
-  import { MarkGithub } from "svelte-octicons";
-  import { setContext } from "svelte";
+  import { MarkGithub, ThreeBars } from "svelte-octicons";
+  import { setContext, afterUpdate } from "svelte";
   import { writable } from "svelte/store";
   import pkg from "../../../package.json";
 
   const tail = writable(undefined);
+
+  let expanded = false;
+  let innerWidth = 0;
 
   setContext("Root", {
     set: current => {
@@ -18,13 +21,70 @@
     }
   });
 
+  afterUpdate(() => {
+    if (expanded && innerWidth > 720) {
+      expanded = false;
+    }
+
+    document.body.className = expanded ? "overflow-y-hidden" : "";
+  });
+
   $: components = Object.keys(Primer);
 </script>
+
+<style>
+  .overlay {
+    position: fixed;
+    z-index: 1;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: calc(100% - 3.75rem);
+    opacity: 0.75;
+  }
+
+  main {
+    max-width: 720px;
+  }
+
+  :global(.mobile-menu) {
+    display: none;
+  }
+
+  @media (max-width: 720px) {
+    main {
+      padding-left: 1rem !important;
+      padding-right: 1rem !important;
+    }
+
+    :global(.mobile-menu) {
+      display: block;
+    }
+
+    aside {
+      position: fixed;
+      z-index: 2;
+      bottom: 0;
+      right: 0;
+      height: calc(100% - 3.75rem);
+      overflow-y: scroll;
+    }
+
+    aside:not(.expanded) {
+      display: none;
+    }
+  }
+</style>
+
+<svelte:window bind:innerWidth />
 
 <Primer.Header.Header>
   <Primer.Header.Item>
     <Primer.Header.Link
-      href={process.env.NODE_ENV === 'development' ? '/' : '/svelte-primer/'}>
+      href={process.env.NODE_ENV === 'development' ? '/' : '/svelte-primer/'}
+      on:click={() => {
+        expanded = false;
+      }}>
       <span class="f4">svelte-primer</span>
       <span class="f6 ml-1 text-gray-light">v{pkg.version}</span>
     </Primer.Header.Link>
@@ -34,22 +94,45 @@
       <MarkGithub width={24} height={24} />
     </Primer.Header.Link>
   </Primer.Header.Item>
+  <Primer.Header.Item class="mobile-menu ml-2 mr-0">
+    <Primer.Button
+      variant="octicon"
+      class="border border-white-fade pl-2 pr-2 rounded-1"
+      aria-label="Menu"
+      aria-expanded={expanded}
+      on:click={() => {
+        expanded = !expanded;
+      }}>
+      <ThreeBars />
+    </Primer.Button>
+  </Primer.Header.Item>
 </Primer.Header.Header>
 
 <div class="d-flex">
-  <aside class="bg-gray-light border-right">
+  <aside class="bg-gray-light border-right" class:expanded>
     <Primer.Navigation.SideNav class="p-3 pr-6">
       <h5 class="text-gray mb-2 pb-1 border-bottom">Components</h5>
       {#each components as component, i (component)}
         <Primer.Navigation.SideNavSubItem
           href="components/{component}/"
+          on:click={() => {
+            expanded = false;
+          }}
           current={segment === 'components' && $tail === component}>
           {component}
         </Primer.Navigation.SideNavSubItem>
       {/each}
     </Primer.Navigation.SideNav>
   </aside>
-  <div class="d-table table-fixed width-full p-6" style="max-width: 740px">
+  <main class="d-table table-fixed width-full p-6">
     <slot />
-  </div>
+  </main>
 </div>
+
+{#if expanded}
+  <div
+    class="overlay bg-gray-dark"
+    on:click={() => {
+      expanded = false;
+    }} />
+{/if}
