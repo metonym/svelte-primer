8b2e3a5b7cf7f5a631c3551e4110a8b89c53fb08
Eric Liu <ericyl.us@gmail.com>
2020-05-22 08:03:28 -0700

    chore(deps-dev): remove unused dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from "rollup-plugin-node-resolve";
 import svelte from "rollup-plugin-svelte";
-import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 import { terser } from "rollup-plugin-terser";
 import { main, module, name } from "./package.json";
 
@@ -14,11 +13,6 @@ export default ["es", "umd"].map((format) => {
       file: UMD ? main : module,
       name: UMD ? name : undefined,
     },
-    plugins: [
-      svelte(),
-      resolve(),
-      sizeSnapshot({ printInfo: false }),
-      UMD && terser(),
-    ],
+    plugins: [svelte(), resolve(), UMD && terser()],
   };
 });
